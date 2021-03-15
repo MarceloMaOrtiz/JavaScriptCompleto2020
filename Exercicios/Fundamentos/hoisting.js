f8d3d7e740d877d6a308a075a399a313bb59c904
MOMachine <marcelomaortiz@hotmail.com>
2021-03-15 17:51:05 -0300

    Fundamentos avançando 3

---
+++ Exercicios/Fundamentos/hoisting.js
@@ -0,0 +1,35 @@
+console.log('a = ', a)
+
+var a = 2
+
+console.log('a = ', a)
+
+
+// Semelhante ao cod de cima, temos
+/*
+var a
+
+console.log('a = ', a)
+
+a = 2
+
+console.log('a = ', a)
+*/
+
+// Com função
+/*
+function teste(){
+    console.log('a = ', a)
+
+    var a = 2
+
+    console.log('a = ', a)
+}
+*/
+
+console.log('b = ', b)
+
+// Let não da hoisting
+let b = 3
+
+console.log('b = ', b)
---
+++ Exercicios/Fundamentos/usandoConst.js
@@ -0,0 +1,9 @@
+const saudacao = 'Opa' // contexto léxico 1
+
+function exec() {
+    const saudacao = 'Falaaa' // contexto léxico 2
+    return saudacao
+}
+
+console.log(saudacao)
+console.log(exec())
